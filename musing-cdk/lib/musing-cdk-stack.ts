82e9ef2d7272a60df7b34b295fde832204204b75
lacier-axle <156736383+lacier-axle@users.noreply.github.com>
2024-03-18 14:47:03 -0700

    [feat] Adding code pipeline to automate builds and kubernetes deployments

--- musing-cdk/lib/musing-cdk-stack.ts
+++ musing-cdk/lib/musing-cdk-stack.ts
@@ -1,71 +1,152 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { KubectlLayer } from 'aws-cdk-lib/lambda-layer-kubectl';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { KubectlV29Layer} from '@aws-cdk/lambda-layer-kubectl-v29';
 
 export class MusingCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-        // Create an ECR repository for the Next.js Docker image
-        const nextJsAppRepo = new cdk.aws_ecr.Repository(this, 'musing-nextjs');
+    const clusterName = "musing-cluster";
 
-        // Create a VPC for the EKS cluster
-        const vpc = new cdk.aws_ec2.Vpc(this, 'musing-vpc', {
-          maxAzs: 3 // Default is all AZs in the region
-        });
-    
-        // Create an EKS cluster
-        const cluster = new cdk.aws_eks.Cluster(this, 'musing-cluster', {
-          version: cdk.aws_eks.KubernetesVersion.V1_29,
-          vpc: vpc,
-          defaultCapacity: 2, // Default is 2 m5.large instances
-          kubectlLayer: new KubectlLayer(this, 'MusingKubectlLayer'),
-        });
-    
-        // Define the Kubernetes deployment for the Next.js app
-        const appLabel = { app: "musing-app" };
-        const deployment = {
-          apiVersion: "apps/v1",
-          kind: "Deployment",
-          metadata: { name: "musing-nextjs-deployment" },
+    // Create a VPC for the EKS cluster
+    const vpc = new cdk.aws_ec2.Vpc(this, 'musing-vpc', {
+      maxAzs: 3 // Default is all AZs in the region
+    });
+
+    // Create an EKS cluster
+    const cluster = new cdk.aws_eks.Cluster(this, clusterName, {
+      version: cdk.aws_eks.KubernetesVersion.V1_29,
+      vpc: vpc,
+      defaultCapacity: 2, // Default is 2 m5.large instances
+      kubectlLayer: new KubectlV29Layer(this, 'MusingKubectlLayer'),
+    });
+
+    const deploymentName = "musing-nextjs-deployment";
+
+    const repoName = "musing-nextjs-repo";
+
+    // Create an ECR repository for the Next.js Docker image
+    const nextJsAppRepo = new cdk.aws_ecr.Repository(this, repoName);
+
+    // Define the Kubernetes deployment for the Next.js app
+    const appLabel = { app: "musing-app" };
+    const deployment = {
+      apiVersion: "apps/v1",
+      kind: "Deployment",
+      metadata: { name: deploymentName },
+      spec: {
+        replicas: 2,
+        selector: { matchLabels: appLabel },
+        template: {
+          metadata: { labels: appLabel },
           spec: {
-            replicas: 2,
-            selector: { matchLabels: appLabel },
-            template: {
-              metadata: { labels: appLabel },
-              spec: {
-                containers: [{
-                  name: "musing-nextjs",
-                  image: `${nextJsAppRepo.repositoryUri}:latest`,
-                  ports: [{ containerPort: 3000 }]
-                }]
-              }
-            }
+            containers: [{
+              name: "musing-nextjs",
+              image: `${nextJsAppRepo.repositoryUri}:latest`,
+              ports: [{ containerPort: 3000 }]
+            }]
           }
-        };
-    
-        // Define the Kubernetes service to expose the Next.js app
-        const service = {
-          apiVersion: "v1",
-          kind: "Service",
-          metadata: { name: "musing-nextjs-service" },
-          spec: {
-            type: "LoadBalancer",
-            ports: [{ port: 80, targetPort: 3000 }],
-            selector: appLabel
-          }
-        };
-    
-        // Add the deployment and service to the EKS cluster
-        cluster.addManifest('MusingNextJsAppDeployment', deployment);
-        cluster.addManifest('MusingNextJsAppService', service);
-    
-        // Output the ECR repository URI
-        new cdk.CfnOutput(this, 'MusingECRRepositoryURI', {
-          value: nextJsAppRepo.repositoryUri,
-        });
-    
-        // TODO Output the Load Balancer URL
+        }
+      }
+    };
+
+    const codeBuildProject = new cdk.aws_codebuild.PipelineProject(this, 'MusingNextJsBuildProject', {
+      environment: {
+        buildImage: cdk.aws_codebuild.LinuxBuildImage.STANDARD_5_0,
+        privileged: true, // Required for Docker builds
+      },
+      buildSpec: cdk.aws_codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          pre_build: {
+            commands: [
+              // Login to Amazon ECR
+              'echo Logging in to Amazon ECR...',
+              '$(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)',
+            ],
+          },
+          build: {
+            commands: [
+              // Build the Docker image
+              'echo Building the Docker image...',
+              'docker build -t $REPOSITORY_URI:latest .',
+              // Tag the Docker image
+              'docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$CODEBUILD_RESOLVED_SOURCE_VERSION',
+            ],
+          },
+          post_build: {
+            commands: [
+              'echo Pushing the Docker image...',
+              'docker push $REPOSITORY_URI:latest',
+              'docker push $REPOSITORY_URI:$CODEBUILD_RESOLVED_SOURCE_VERSION',
+              // Add kubectl command to update the Kubernetes deployment
+              `aws eks --region $AWS_DEFAULT_REGION update-kubeconfig --name ${clusterName}`,
+              `kubectl set image deployment/${deploymentName} ${repoName}=$REPOSITORY_URI:$CODEBUILD_RESOLVED_SOURCE_VERSION`,
+            ],
+          },
+        },
+        variables: {
+          REPOSITORY_URI: `${nextJsAppRepo.repositoryUri}`,
+        },
+      }),
+    });
+
+    // Grant permissions to CodeBuild to push images to ECR
+    nextJsAppRepo.grantPullPush(codeBuildProject.grantPrincipal);
+
+    const sourceOutput = new cdk.aws_codepipeline.Artifact();
+    const sourceAction = new cdk.aws_codepipeline_actions.GitHubSourceAction({
+      actionName: 'GitHub',
+      output: sourceOutput,
+      oauthToken: cdk.SecretValue.secretsManager('GITHUB_TOKEN'),
+      owner: 'lacier-axle',
+      repo: 'musing',
+      branch: 'main',
+    });
+
+    const buildAction = new cdk.aws_codepipeline_actions.CodeBuildAction({
+      actionName: 'BuildAndPush',
+      project: codeBuildProject,
+      input: sourceOutput,
+    });
+
+    const musingCodePipeline = new cdk.aws_codepipeline.Pipeline(this, 'Pipeline', {
+      pipelineName: 'MusingNextJsDeployment',
+      stages: [
+        {
+          stageName: 'Source',
+          actions: [sourceAction],
+        },
+        {
+          stageName: 'Build',
+          actions: [buildAction],
+        },
+        // Define deploy stage with Lambda or a custom action to update EKS deployment
+      ],
+    });
+
+
+    // Define the Kubernetes service to expose the Next.js app
+    const service = {
+      apiVersion: "v1",
+      kind: "Service",
+      metadata: { name: "musing-nextjs-service" },
+      spec: {
+        type: "LoadBalancer",
+        ports: [{ port: 80, targetPort: 3000 }],
+        selector: appLabel
+      }
+    };
+
+    // Add the deployment and service to the EKS cluster
+    cluster.addManifest('MusingNextJsAppDeployment', deployment);
+    cluster.addManifest('MusingNextJsAppService', service);
+
+    // Output the ECR repository URI
+    new cdk.CfnOutput(this, 'MusingECRRepositoryURI', {
+      value: nextJsAppRepo.repositoryUri,
+    });
+
+    // TODO Output the Load Balancer URL
   }
 }
